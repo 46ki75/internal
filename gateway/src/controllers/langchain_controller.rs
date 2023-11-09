e3b3c4671747e53d5a510c8d45197fe00e79f4d4
shirayuki <shirayuki@46ki75.com>
2023-11-10 02:21:57 +0900

    feat(controllers): separate aggregation controller for Langchain service

---
+++ gateway/src/controllers/langchain_controller.rs
@@ -0,0 +1,54 @@
+use actix_web::{web, Error, HttpRequest, HttpResponse};
+use reqwest::Client;
+use std::sync::Arc;
+
+pub async fn forward_request(
+    client: web::Data<Arc<Client>>,
+    req: HttpRequest,
+    body: web::Bytes,
+) -> Result<HttpResponse, Error> {
+    let url = format!("http://localhost:10002{}", req.uri());
+    let forwarded_request = client.request(req.method().clone(), &url);
+
+    let mut request_builder = forwarded_request.body(body);
+    for (header_name, header_value) in req.headers() {
+        match header_name.as_str() {
+            "host" | "authorization" => continue,
+            _ => {
+                if let Ok(value_str) = header_value.to_str() {
+                    request_builder = request_builder.header(header_name.clone(), value_str);
+                }
+            }
+        }
+    }
+
+    let response = request_builder.send().await.map_err(|e| {
+        eprintln!("Failed to send request: {}", e);
+        actix_web::error::ErrorInternalServerError("Failed to send request")
+    })?;
+
+    let mut response_builder = HttpResponse::build(response.status());
+    for (header_name, header_value) in response.headers() {
+        response_builder.insert_header((header_name.clone(), header_value.clone()));
+    }
+
+    let response_body = response.bytes().await.map_err(|e| {
+        eprintln!("Failed to read response: {}", e);
+        actix_web::error::ErrorInternalServerError("Failed to read response")
+    })?;
+
+    Ok(response_builder.body(response_body))
+}
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/api/langchain")
+            .route(web::get().to(forward_request))
+            .route(web::post().to(forward_request)),
+    )
+    .service(
+        web::resource("/api/langchain/{tail:.*}")
+            .route(web::get().to(forward_request))
+            .route(web::post().to(forward_request)),
+    );
+}
--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -1,46 +1,23 @@
+mod controllers;
+
 use actix_files as fs;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Result};
+use actix_web::{middleware::Logger, web, App, HttpServer};
+use controllers::langchain_controller;
 use reqwest::Client;
-
-async fn forward_request(req: HttpRequest, body: web::Bytes) -> Result<HttpResponse> {
-    let client = Client::new();
-    let url = format!("http://localhost:10002{}", req.uri());
-
-    println!("{}", url);
-
-    let mut forwarded_request = client.request(req.method().clone(), &url);
-    for (header_name, header_value) in req.headers() {
-        forwarded_request =
-            forwarded_request.header(header_name.as_str(), header_value.to_str().unwrap());
-    }
-
-    let response = forwarded_request.body(body).send().await.map_err(|e| {
-        eprintln!("Failed to send request: {}", e);
-        actix_web::error::ErrorInternalServerError("Failed to send request")
-    })?;
-
-    let mut response_builder = HttpResponse::build(response.status());
-    for (header_name, header_value) in response.headers() {
-        response_builder.append_header((header_name, header_value));
-    }
-
-    let response_body = response.bytes().await.map_err(|e| {
-        eprintln!("Failed to read response: {}", e);
-        actix_web::error::ErrorInternalServerError("Failed to read response")
-    })?;
-
-    Ok(response_builder.body(response_body))
-}
+use std::sync::Arc;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let client = Arc::new(Client::new());
+
+    HttpServer::new(move || {
         App::new()
-            .route("/api/langchain", web::get().to(forward_request))
-            .route("/api/langchain/{tail:.*}", web::to(forward_request))
+            .wrap(Logger::default())
+            .app_data(web::Data::new(client.clone()))
+            .configure(langchain_controller::config)
             .service(fs::Files::new("/", "./dist").index_file("index.html"))
     })
-    .bind(("0.0.0.0", 10000))?
+    .bind("0.0.0.0:10000")?
     .run()
     .await
 }

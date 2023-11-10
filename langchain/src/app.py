e6044cafa256842bdfcb8fe1a07d89939bde637d
shirayuki <shirayuki@46ki75.com>
2023-11-11 03:56:35 +0900

    feat(web): switch web framework to FastAPI

--- langchain/src/app.py
+++ langchain/src/app.py
@@ -1,43 +1,41 @@
-from flask import Flask, jsonify, Response, request
-from models.ResponseBuilder import NormalResponseBuilder
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
 import os
 import time
 
-app = Flask(__name__)
+app = FastAPI()
 
 start_time = time.time()
 
 
-@app.route("/api/langchain")
-def get_langchain() -> Response:
-    response = (
-        NormalResponseBuilder()
-        .push_data({"message": "Hello, Langchain!"})
-        .self_link(request.url)
-        .build()
-    )
-    return jsonify(response)
+class NormalResponse(BaseModel):
+    data: dict
+    links: dict
 
 
-@app.route("/api/langchain/up")
-def get_up() -> Response:
+def build_normal_response(data: dict, self_link: str) -> NormalResponse:
+    return NormalResponse(data=data, links={"self": self_link})
+
+
+@app.get("/api/langchain")
+def get_langchain(request: Request) -> NormalResponse:
+    response_data = {"message": "Hello, Langchain!"}
+    return build_normal_response(response_data, request.url_for("get_langchain"))
+
+
+@app.get("/api/langchain/up")
+def get_up(request: Request) -> NormalResponse:
     uptime_seconds = time.time() - start_time
-    response = (
-        NormalResponseBuilder()
-        .push_data(
-            {
-                "status": "up",
-                "uptime": str(int(uptime_seconds)) + "s",
-                "hostname": os.getenv("HOSTNAME", "unknown"),
-                "time": time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()),
-            }
-        )
-        .self_link(request.url)
-        .build()
-    )
-
-    return jsonify(response)
+    response_data = {
+        "status": "up",
+        "uptime": str(int(uptime_seconds)) + "s",
+        "hostname": os.getenv("HOSTNAME", "unknown"),
+        "time": time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()),
+    }
+    return build_normal_response(response_data, request.url_for("get_up"))
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=10002)
+    import uvicorn  # type: ignore
+
+    uvicorn.run(app, host="0.0.0.0", port=10002)

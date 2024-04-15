6312830b6b11fa6d0a6a9ecce9475242b4fd684c
shirayuki <shirayuki@46ki75.com>
2024-04-16 04:46:11 +0900

    refactor(cdk): remove unnecessary comments

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -4,21 +4,11 @@ import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-
     const lambda = new NodejsFunction(this, "Lambda", {
-      // projectRoot: "..",
       entry: "../.output/server/index.mjs",
       handler: "handler",
       runtime: Runtime.NODEJS_20_X,
@@ -30,13 +20,11 @@ export class CdkStack extends cdk.Stack {
       createDefaultStage: true,
     });
 
-    // Lambda統合の設定
     const lambdaIntegration = new HttpLambdaIntegration(
       "LambdaIntegration",
       lambda
     );
 
-    // ルートの追加
     httpApi.addRoutes({
       path: "/{all+}",
       methods: [HttpMethod.ANY],

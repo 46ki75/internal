62a8ba5111c13ae86cdb1b3e49e8b4b6debfaa17
shirayuki <shirayuki@46ki75.com>
2024-04-19 05:02:26 +0000

    feat(auth): implement login and logout functionality

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,30 @@
 import { createApp } from 'vue'
-import './style.css'
+import './style.scss'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// vue-router
+import router from './router'
+
+// tanstack-query
+import { VueQueryPlugin } from '@tanstack/vue-query'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'default',
+    themes: { default: { colors: { primary: 'rgb(40,40,40)' } } }
+  }
+})
+
+const app = createApp(App)
+app.use(router)
+app.use(vuetify)
+app.use(VueQueryPlugin)
+app.mount('#app')
---
+++ web/src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>HOME INDEX</div>
+  <router-link to="/login">Login</router-link>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>

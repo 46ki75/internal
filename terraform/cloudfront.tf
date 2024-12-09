eccc8e89441e3184f0744381354448834749b739
shirayuki <shirayuki@46ki75.com>
2024-12-10 08:25:08 +0900

    feat(terraform): add CloudFront configuration for GraphQL API with custom cache behavior

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -46,10 +46,50 @@ resource "aws_cloudfront_distribution" "default" {
     }
   }
 
+  ordered_cache_behavior {
+    path_pattern = "/graphql"
+    allowed_methods = [
+      "DELETE",
+      "GET",
+      "HEAD",
+      "OPTIONS",
+      "PATCH",
+      "POST",
+      "PUT"
+    ]
+    cached_methods         = ["GET", "HEAD"]
+    viewer_protocol_policy = "redirect-to-https"
+    target_origin_id       = "api-backend"
+
+    default_ttl = 0
+    min_ttl     = 0
+    max_ttl     = 0
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "none"
+      }
+      headers = ["Authorization"]
+    }
+  }
+
   origin {
     domain_name              = aws_s3_bucket.web.bucket_regional_domain_name
     origin_id                = "s3-web"
     origin_access_control_id = aws_cloudfront_origin_access_control.web.id
+  }
+
+  origin {
+    domain_name = aws_apigatewayv2_domain_name.backend.domain_name
+    origin_id   = "api-backend"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
   }
 
   default_root_object = "index.html"

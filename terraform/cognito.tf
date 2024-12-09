1e7243137eb4dd13af6052a86dc38d8c17633206
shirayuki <shirayuki@46ki75.com>
2024-12-09 10:24:23 +0900

    feat(terraform): rename Cognito user pool resource and add user pool client with outputs

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -1,8 +1,22 @@
-resource "aws_cognito_user_pool" "pool" {
+resource "aws_cognito_user_pool" "default" {
   name = "${terraform.workspace}-46ki75-internal-cognito-userpool"
 
   # Disable self sign-up
   admin_create_user_config {
     allow_admin_create_user_only = true
   }
+}
+
+output "user_pool_id" {
+  value = aws_cognito_user_pool.default.id
+}
+
+resource "aws_cognito_user_pool_client" "default" {
+  user_pool_id    = aws_cognito_user_pool.default.id
+  name            = "${terraform.workspace}-46ki75-internal-cognito-userpool-client"
+  generate_secret = false # For SPA
+}
+
+output "user_pool_client_id" {
+  value = aws_cognito_user_pool_client.default.id
 }

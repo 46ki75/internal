8c357b7b7e74eed3652f443936de19c12c943332
shirayuki <shirayuki@46ki75.com>
2024-12-09 21:58:07 +0900

    feat(terraform): implement API Gateway integration and routing for GraphQL backend

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -1,46 +1,35 @@
 resource "aws_apigatewayv2_api" "backend" {
   name          = "${terraform.workspace}-46ki75-apigwv2-http-backend"
   protocol_type = "HTTP"
-  route_key     = "ANY /graphql"
-  target        = aws_lambda_alias.graphql.invoke_arn
 }
 
-# resource "aws_apigatewayv2_integration" "backend" {
-#   api_id           = aws_apigatewayv2_api.backend.id
-#   integration_type = "AWS_PROXY"
+resource "aws_apigatewayv2_integration" "backend" {
+  api_id           = aws_apigatewayv2_api.backend.id
+  integration_type = "AWS_PROXY"
 
-#   connection_type = "INTERNET"
-#   integration_uri = aws_lambda_alias.graphql.invoke_arn
-# }
+  connection_type = "INTERNET"
+  integration_uri = aws_lambda_alias.graphql.invoke_arn
+}
 
-# resource "aws_apigatewayv2_route" "backend" {
-#   api_id    = aws_apigatewayv2_api.backend.id
-#   route_key = "ANY /graphql"
-#   target    = "integrations/${aws_apigatewayv2_integration.backend.id}"
-# }
+resource "aws_apigatewayv2_route" "backend" {
+  api_id    = aws_apigatewayv2_api.backend.id
+  route_key = "ANY /graphql"
+  target    = "integrations/${aws_apigatewayv2_integration.backend.id}"
+}
 
-# resource "aws_apigatewayv2_stage" "backend" {
-#   api_id      = aws_apigatewayv2_api.backend.id
-#   name        = "deafult"
-#   auto_deploy = true
+resource "aws_apigatewayv2_stage" "backend" {
+  api_id      = aws_apigatewayv2_api.backend.id
+  name        = terraform.workspace
+  auto_deploy = true
 
-#   route_settings {
-#     route_key              = aws_apigatewayv2_route.backend.route_key
-#     throttling_burst_limit = 100000
-#     throttling_rate_limit  = 100000
-#   }
 
-#   access_log_settings {
-#     destination_arn = aws_cloudwatch_log_group.apigw_log_group.arn
-#     format          = "{ \"requestId\":\"$context.requestId\", \"ip\": \"$context.identity.sourceIp\", \"requestTime\":\"$context.requestTime\", \"httpMethod\":\"$context.httpMethod\",\"routeKey\":\"$context.routeKey\", \"status\":\"$context.status\",\"protocol\":\"$context.protocol\", \"responseLength\":\"$context.responseLength\" }"
-#   }
-# }
+  route_settings {
+    route_key              = aws_apigatewayv2_route.backend.route_key
+    throttling_burst_limit = 100000
+    throttling_rate_limit  = 100000
+  }
 
-# resource "aws_cloudwatch_log_group" "apigw_log_group" {
-#   name              = "${terraform.workspace}-46ki75-apigwv2-log-group"
-#   retention_in_days = 7
-#   skip_destroy      = false
-# }
+}
 
 resource "aws_lambda_permission" "apigwv2" {
   statement_id  = "AllowExecutionFromAPIGateway"

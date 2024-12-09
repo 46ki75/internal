07a4ffbdfb85f96eaa8a8c5c2e1588f88493c3b0
shirayuki <shirayuki@46ki75.com>
2024-12-09 18:58:53 +0900

    feat(terraform): update IAM roles and policies for GraphQL Lambda function

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "lambda_role" {
+resource "aws_iam_role" "lambda_role_graphql" {
   name = "lambda_role"
   assume_role_policy = jsonencode({
     "Version" : "2012-10-17",
@@ -14,7 +14,7 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-resource "aws_iam_policy" "lambda_policy" {
+resource "aws_iam_policy" "lambda_policy_graphql" {
   name        = "lambda_policy"
   description = "Allow lambda to access cloudwatch logs"
   policy = jsonencode({
@@ -33,18 +33,23 @@ resource "aws_iam_policy" "lambda_policy" {
   })
 }
 
-resource "aws_lambda_function" "mock" {
-  function_name = "${terraform.workspace}-lambda"
-  role          = aws_iam_role.lambda_role.arn
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment_graphql" {
+  role       = aws_iam_role.lambda_role_graphql.name
+  policy_arn = aws_iam_policy.lambda_policy_graphql.arn
+}
+
+resource "aws_lambda_function" "graphql" {
+  function_name = "${terraform.workspace}-46ki75-lambda-function-graphql"
+  role          = aws_iam_role.lambda_role_graphql.arn
   filename      = "./assets/bootstrap.zip"
-  handler       = "mock.handler"
+  handler       = "bootstrap.handler"
   runtime       = "provided.al2023"
   architectures = ["x86_64"]
   publish       = true # Publish a new version
 }
 
-resource "aws_lambda_alias" "mock" {
+resource "aws_lambda_alias" "graphql" {
   name             = "stable"
-  function_name    = aws_lambda_function.mock.function_name
-  function_version = aws_lambda_function.mock.version
+  function_name    = aws_lambda_function.graphql.function_name
+  function_version = aws_lambda_function.graphql.version
 }

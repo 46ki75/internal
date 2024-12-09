c126bee3443509f2638ff6abc9beabb94c7c3c4d
shirayuki <shirayuki@46ki75.com>
2024-12-09 18:14:07 +0900

    feat(web): add deployment scripts and configure CloudFront and S3 resources

---
+++ terraform/cloudfront.tf
@@ -0,0 +1,60 @@
+resource "aws_cloudfront_origin_access_control" "web" {
+  name                              = "web"
+  description                       = "Frontend S3 OAC"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "default" {
+  enabled = true
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  default_cache_behavior {
+    allowed_methods = [
+      "DELETE",
+      "GET",
+      "HEAD",
+      "OPTIONS",
+      "PATCH",
+      "POST",
+      "PUT"
+    ]
+    cached_methods         = ["GET", "HEAD"]
+    viewer_protocol_policy = "redirect-to-https"
+    target_origin_id       = "s3-web"
+
+    default_ttl = 3600 * 24 * 30
+    min_ttl     = 0
+    max_ttl     = 3600 * 24 * 30 * 12
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+      headers = ["etag"]
+    }
+  }
+
+  origin {
+    domain_name              = aws_s3_bucket.web.bucket_regional_domain_name
+    origin_id                = "s3-web"
+    origin_access_control_id = aws_cloudfront_origin_access_control.web.id
+  }
+
+  default_root_object = "index.html"
+}
+
+output "cloudfront_domain" {
+  value = aws_cloudfront_distribution.default.domain_name
+}

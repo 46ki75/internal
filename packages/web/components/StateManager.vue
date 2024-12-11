a49a338a6ffcc9ec82e1e699f7403bf1d90952db
shirayuki <shirayuki@46ki75.com>
2024-12-11 19:37:36 +0900

    feat(auth): add loading indicator during access token refresh process

--- packages/web/components/StateManager.vue
+++ packages/web/components/StateManager.vue
@@ -1,18 +1,38 @@
+<template>
+  <div class="fix" v-if="refreshing">
+    <ElmDotLoadingIcon /> <ElmInlineText text="アクセストークンを更新中" />
+  </div>
+</template>
+
 <script setup lang="ts">
+import { ElmDotLoadingIcon, ElmInlineText } from '@elmethis/core'
+
 const router = useRouter()
 const authStore = useAuthStore()
+
+const refreshing = ref(false)
 
 onMounted(() => {
   window.setInterval(async () => {
     try {
+      refreshing.value = true
       await authStore.refreshAccessToken()
       console.log('Access token refreshed.')
     } catch {
       window.alert('Session expired. Please sign in again.')
       router.push('/login')
+    } finally {
+      refreshing.value = false
     }
   }, 1000 * 60)
 })
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.fix {
+  margin: 0.5rem;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+}
+</style>

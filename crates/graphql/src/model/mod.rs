3cec520bad9e2a3c9fb65a3ca2d43eeece5315a4
shirayuki <shirayuki@46ki75.com>
2024-12-11 15:38:26 +0900

    feat(graphql): add RelayConnection, RelayEdge, and PageInfo structures for GraphQL pagination

--- crates/graphql/src/model/mod.rs
+++ crates/graphql/src/model/mod.rs
@@ -1,3 +1,23 @@
 pub mod anki;
 pub mod bookmark;
 pub mod translation;
+
+#[derive(async_graphql::SimpleObject)]
+pub struct RelayConnection<T: async_graphql::OutputType> {
+    pub edges: Vec<RelayEdge<T>>,
+    pub page_info: PageInfo,
+}
+
+#[derive(async_graphql::SimpleObject)]
+pub struct RelayEdge<T: async_graphql::OutputType> {
+    pub node: T,
+    pub cursor: String,
+}
+
+#[derive(async_graphql::SimpleObject)]
+pub struct PageInfo {
+    pub has_next_page: bool,
+    pub has_previous_page: bool,
+    pub start_cursor: Option<String>,
+    pub end_cursor: Option<String>,
+}

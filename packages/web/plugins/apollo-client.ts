bf876fe5dd71d47ada2488ddba61b4a7c3067d46
shirayuki <shirayuki@46ki75.com>
2024-12-12 16:40:37 +0900

    feat(apollo): refactor Apollo Client setup and remove deprecated utility file

--- packages/web/plugins/apollo-client.ts
+++ packages/web/plugins/apollo-client.ts
@@ -1,6 +1,11 @@
 import { defineNuxtPlugin } from '#app'
 import { provideApolloClient } from '@vue/apollo-composable'
-import { apolloClient } from '~/utils/apollo'
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+
+export const apolloClient = new ApolloClient({
+  uri: 'http://localhost:20000/api/graphql',
+  cache: new InMemoryCache()
+})
 
 export default defineNuxtPlugin((nuxtApp) => {
   // Provide the Apollo Client globally

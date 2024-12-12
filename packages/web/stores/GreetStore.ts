0691e999e2e4cdadf688826eebf908b229a87934
shirayuki <shirayuki@46ki75.com>
2024-12-12 21:07:55 +0900

    feat(GreetStore): refactor GraphQL query formatting in GreetStore and gql.ts for consistency

--- packages/web/stores/GreetStore.ts
+++ packages/web/stores/GreetStore.ts
@@ -3,14 +3,14 @@ import { useQuery } from '@vue/apollo-composable'
 
 import { graphql } from '../graphql'
 
+const GET_HELLO = graphql(`
+  query SampleQuery {
+    greet
+  }
+`)
+
 export const useGreetStore = defineStore('greet', {
   state: () => {
-    const GET_HELLO = graphql(`
-      query SampleQuery {
-        greet
-      }
-    `)
-
     const { result } = useQuery(GET_HELLO)
 
     return { result }

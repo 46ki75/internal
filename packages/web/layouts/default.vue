c2a10ae89cb118de6f0e0612e274b2284cdeed8f
shirayuki <shirayuki@46ki75.com>
2024-12-14 13:09:38 +0900

    feat: add fade-in animation to main layout for improved user experience

--- packages/web/layouts/default.vue
+++ packages/web/layouts/default.vue
@@ -1,13 +1,27 @@
 <template>
   <Header />
+
   <main class="main">
     <slot />
   </main>
 </template>
 
 <style scoped lang="scss">
+@keyframes fade {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
 .main {
   box-sizing: border-box;
   padding: 0.5rem;
+  animation-name: fade;
+  animation-duration: 500ms;
+  animation-fill-mode: both;
+  animation-iteration-count: 1;
 }
 </style>

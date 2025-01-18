6e26d8ada1b37e5c48daeadfb4f5b48349c92c12
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-01-19 00:14:23 +0900

    feat(aws): add CloudWatch alarms and update SNS topic policy for error notifications

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -1,12 +1,11 @@
 resource "aws_cloudwatch_event_rule" "lambda_routine" {
-  name        = "${terraform.workspace}-46ki75-eventbridge-lambda-routine"
+  name        = "${terraform.workspace}-46ki75-internal-eventbridge-lambda-routine"
   description = "Trigger lambda routine"
 
   schedule_expression = "cron(0 15 * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "lambda_routine" {
-  rule      = aws_cloudwatch_event_rule.lambda_routine.name
-  target_id = "${terraform.workspace}-46ki75-lambda-routine"
-  arn       = aws_lambda_alias.routine.arn
+  rule = aws_cloudwatch_event_rule.lambda_routine.name
+  arn  = aws_lambda_alias.routine.arn
 }

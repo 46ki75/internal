e17b15a2b0959c30f523968985e628de6a7597d2
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-01-25 19:44:11 +0900

    feat: add S3 bucket component for resource management

--- pulumi/cmd/main.go
+++ pulumi/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"internal/pkg/dynamodb"
 	"internal/pkg/route53"
+	"internal/pkg/s3"
 	"internal/pkg/util"
 
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
@@ -29,6 +30,15 @@ func main() {
 			ctx,
 			"DynamoDbComponent",
 			&dynamodb.DynamoDbComponentArgs{},
+		)
+		if err != nil {
+			return err
+		}
+
+		_, err = s3.NewS3BucketComponent(
+			ctx,
+			"S3BucketComponent",
+			&s3.S3BucketComponentArgs{},
 		)
 		if err != nil {
 			return err
---
+++ pulumi/pkg/s3/bucket.go
@@ -0,0 +1,45 @@
+package s3
+
+import (
+	"fmt"
+
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/s3"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+type S3BucketComponent struct {
+	pulumi.ResourceState
+	S3Bucket *s3.BucketV2
+}
+
+type S3BucketComponentArgs struct {
+}
+
+func NewS3BucketComponent(
+	ctx *pulumi.Context,
+	name string,
+	args *S3BucketComponentArgs,
+	opts ...pulumi.ResourceOption,
+) (*S3BucketComponent, error) {
+	component := &S3BucketComponent{}
+	stackName := ctx.Stack()
+
+	err := ctx.RegisterComponentResource(
+		"46ki75:component:S3Bucket",
+		name, component, opts...,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	component.S3Bucket, err = s3.NewBucketV2(
+		ctx,
+		fmt.Sprintf("%s-46ki75-internal-s3-bucket-web", stackName),
+		&s3.BucketV2Args{},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return component, nil
+}

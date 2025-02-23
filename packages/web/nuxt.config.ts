7c0f3ed2e48974d9fa22e5e9f8fd00845dac7951
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-02-23 01:29:36 +0000

    refactor(graphql): rename internal_graphql to internal-graphql for consistency and update related configurations

--- packages/web/nuxt.config.ts
+++ packages/web/nuxt.config.ts
@@ -1,55 +1,55 @@
-import { fetchSSMParameter } from './utils/fetchSsmParameter'
+import { fetchSSMParameter } from "./utils/fetchSsmParameter";
 
 if (process.env.ENVIRONMENT == null) {
-  throw new Error('ENVIRONMENT is not set')
+  throw new Error("ENVIRONMENT is not set");
 } else {
-  console.log(`ENVIRONMENT: ${process.env.ENVIRONMENT}`)
+  console.log(`ENVIRONMENT: ${process.env.ENVIRONMENT}`);
 }
 
 const USER_POOL_ID = await fetchSSMParameter(
   `/${process.env.ENVIRONMENT}/46ki75/internal/cognito/userpool/id`
-)
+);
 
 const USER_POOL_CLIENT_ID = await fetchSSMParameter(
   `/${process.env.ENVIRONMENT}/46ki75/internal/cognito/userpool/client/id`
-)
+);
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   devServer: { port: 20000 },
   vite: {
     server: {
       proxy: {
-        '/api/graphql': {
-          target: 'http://localhost:20010/lambda-url/graphql',
-          changeOrigin: true
-        }
-      }
+        "/api/graphql": {
+          target: "http://localhost:20010/lambda-url/internal-graphql",
+          changeOrigin: true,
+        },
+      },
     },
     css: {
       preprocessorOptions: {
         scss: {
-          api: 'modern-compiler'
-        }
-      }
-    }
+          api: "modern-compiler",
+        },
+      },
+    },
   },
-  modules: ['@pinia/nuxt'],
+  modules: ["@pinia/nuxt"],
   runtimeConfig: {
     public: {
       USER_POOL_ID,
-      USER_POOL_CLIENT_ID
-    }
+      USER_POOL_CLIENT_ID,
+    },
   },
   postcss: {
     plugins: {
       cssnano: {},
       autoprefixer: {},
-      'postcss-preset-env': {
-        stage: 3
-      }
-    }
-  }
-})
+      "postcss-preset-env": {
+        stage: 3,
+      },
+    },
+  },
+});

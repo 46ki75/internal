9323269b58c37cc96d5ea3fce1e60d9b114ae626
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-02-23 15:56:27 +0000

    feat(graphql): add debug logging to Lambda function and update development command

--- crates/graphql/src/lib.rs
+++ crates/graphql/src/lib.rs
@@ -12,16 +12,20 @@ use lambda_http::{http::Method, Body, Error, Request, Response};
 use serde_json::json;
 
 pub async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+    log::debug!("Starting Lambda function");
+
     dotenvy::dotenv().ok();
 
     std::env::var("NOTION_API_KEY").map_err(|_| Error::from("NOTION_API_KEY not found"))?;
 
+    log::debug!("Injecting dependencies: Anki");
     let anki_repository = std::sync::Arc::new(crate::repository::anki::AnkiRepositoryImpl);
     let anki_service = std::sync::Arc::new(crate::service::anki::AnkiService { anki_repository });
     let anki_query_resolver = std::sync::Arc::new(crate::resolver::anki::query::AnkiQueryResolver);
     let anki_mutation_resolver =
         std::sync::Arc::new(crate::resolver::anki::mutation::AnkiMutationResolver);
 
+    log::debug!("Injecting dependencies: Bookmark");
     let bookmark_repository =
         std::sync::Arc::new(crate::repository::bookmark::BookmarkRepositoryImpl);
     let bookmark_service = std::sync::Arc::new(crate::service::bookmark::BookmarkService {
@@ -30,15 +34,18 @@ pub async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     let bookmark_query_resolver =
         std::sync::Arc::new(crate::resolver::bookmark::query::BookmarkQueryResolver);
 
+    log::debug!("Building schema: QueryRoot");
     let query_root = crate::query::QueryRoot {
         anki_query_resolver,
         bookmark_query_resolver,
     };
 
+    log::debug!("Building schema: MutationRoot");
     let mutation_root = crate::mutation::MutationRoot {
         anki_mutation_resolver,
     };
 
+    log::debug!("Building schema: Schema");
     let schema = Schema::build(query_root, mutation_root, EmptySubscription)
         .data(event.headers().clone())
         .data(anki_service)

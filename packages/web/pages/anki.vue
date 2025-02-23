9fe1c701e020cb142c206df162078b35267a616e
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-02-23 16:43:17 +0000

    refactor(graphql): remove Translate component and related files from the project

--- packages/web/pages/anki.vue
+++ packages/web/pages/anki.vue
@@ -1,16 +1,11 @@
 <template>
   <main class="main">
-    <div class="translate">
-      <ElmToggle summary="Translation">
-        <Translate />
-      </ElmToggle>
-    </div>
     <Anki />
   </main>
 </template>
 
 <script setup lang="ts">
-import { ElmToggle } from '@elmethis/core'
+import { ElmToggle } from "@elmethis/core";
 </script>
 
 <style scoped lang="scss">
@@ -19,11 +14,5 @@ import { ElmToggle } from '@elmethis/core'
   flex-direction: column;
   justify-content: center;
   align-items: center;
-}
-
-.translate {
-  width: 100%;
-  max-width: 800px;
-  margin-bottom: 2rem;
 }
 </style>

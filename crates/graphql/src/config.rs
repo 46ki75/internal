21eadd0c9a570b07e657bae7902ab7deba42c2e5
Shirayuki Chomolungma <shirayuki@46ki75.com>
2025-03-01 11:37:27 +0000

    feat(graphql): integrate AWS SSM for fetching Notion API keys and database IDs

--- crates/graphql/src/config.rs
+++ crates/graphql/src/config.rs
@@ -14,34 +14,83 @@ pub struct Config {
 
 impl Config {
     pub async fn try_new_async() -> Result<Self, crate::error::Error> {
-        let environment = std::env::var("ENVIRONMENT").map_err(|_| {
-            crate::error::Error::EnvironmentalVariableNotFound("ENVIRONMENT".to_string())
-        })?;
-
         dotenvy::dotenv().ok();
 
-        let notion_api_key = std::env::var("NOTION_API_KEY").map_err(|_| {
-            crate::error::Error::EnvironmentalVariableNotFound("NOTION_API_KEY".to_string())
+        let stage_name = std::env::var("STAGE_NAME").map_err(|_| {
+            crate::error::Error::EnvironmentalVariableNotFound("STAGE_NAME".to_string())
         })?;
 
-        let notion_anki_database_id = std::env::var("NOTION_ANKI_DATABASE_ID").map_err(|_| {
-            crate::error::Error::EnvironmentalVariableNotFound(
-                "NOTION_ANKI_DATABASE_ID".to_string(),
-            )
-        })?;
+        log::info!("STAGE_NAME: {}", stage_name);
 
-        let notion_to_do_database_id = std::env::var("NOTION_TO_DO_DATABASE_ID").map_err(|_| {
-            crate::error::Error::EnvironmentalVariableNotFound(
-                "NOTION_TO_DO_DATABASE_ID".to_string(),
-            )
-        })?;
+        let aws_sdk_config = aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await;
 
-        let notion_bookmark_database_id =
-            std::env::var("NOTION_BOOKMARK_DATABASE_ID").map_err(|_| {
-                crate::error::Error::EnvironmentalVariableNotFound(
-                    "NOTION_BOOKMARK_DATABASE_ID".to_string(),
-                )
-            })?;
+        let dynamodb_client = std::sync::Arc::new(aws_sdk_dynamodb::Client::new(&aws_sdk_config));
+
+        let ssm_client = std::sync::Arc::new(aws_sdk_ssm::Client::new(&aws_sdk_config));
+
+        let notion_api_key = ssm_client
+            .get_parameter()
+            .name(format!("/{stage_name}/46ki75/internal/notion/secret"))
+            .with_decryption(true)
+            .send()
+            .await
+            .map_err(|e| crate::error::Error::SsmFetchParameter(e.to_string()))?
+            .parameter
+            .ok_or(crate::error::Error::SsmFetchParameter(
+                "No parameter found".to_string(),
+            ))?
+            .value
+            .ok_or(crate::error::Error::SsmFetchParameter(
+                "No value found".to_string(),
+            ))?;
+
+        let notion_anki_database_id = ssm_client
+            .get_parameter()
+            .name("/shared/46ki75/internal/notion/anki/database/id")
+            .with_decryption(true)
+            .send()
+            .await
+            .map_err(|e| crate::error::Error::SsmFetchParameter(e.to_string()))?
+            .parameter
+            .ok_or(crate::error::Error::SsmFetchParameter(
+                "No parameter found".to_string(),
+            ))?
+            .value
+            .ok_or(crate::error::Error::SsmFetchParameter(
+                "No value found".to_string(),
+            ))?;
+
+        let notion_to_do_database_id = ssm_client
+            .get_parameter()
+            .name("/shared/46ki75/internal/notion/todo/database/id")
+            .with_decryption(true)
+            .send()
+            .await
+            .map_err(|e| crate::error::Error::SsmFetchParameter(e.to_string()))?
+            .parameter
+            .ok_or(crate::error::Error::SsmFetchParameter(
+                "No parameter found".to_string(),
+            ))?
+            .value
+            .ok_or(crate::error::Error::SsmFetchParameter(
+                "No value found".to_string(),
+            ))?;
+
+        let notion_bookmark_database_id = ssm_client
+            .get_parameter()
+            .name("/shared/46ki75/internal/notion/bookmark/database/id")
+            .with_decryption(true)
+            .send()
+            .await
+            .map_err(|e| crate::error::Error::SsmFetchParameter(e.to_string()))?
+            .parameter
+            .ok_or(crate::error::Error::SsmFetchParameter(
+                "No parameter found".to_string(),
+            ))?
+            .value
+            .ok_or(crate::error::Error::SsmFetchParameter(
+                "No value found".to_string(),
+            ))?;
 
         let notion_client =
             std::sync::Arc::new(notionrs::client::Client::new().secret(&notion_api_key));
@@ -49,12 +98,8 @@ impl Config {
         let elmethis_notion_client =
             std::sync::Arc::new(elmethis_notion::client::Client::new(&notion_api_key));
 
-        let aws_sdk_config = aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await;
-
-        let dynamodb_client = std::sync::Arc::new(aws_sdk_dynamodb::Client::new(&aws_sdk_config));
-
         Ok(Config {
-            environment,
+            environment: stage_name,
             notion_api_key,
             notion_anki_database_id,
             notion_to_do_database_id,

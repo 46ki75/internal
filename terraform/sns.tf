39ce23e57bec2ed79259212546d6b3a1fd88a3c8
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-04-08 08:03:59 +0000

    feat: Add SNS permission and subscription for notion notification lambda function

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -25,3 +25,9 @@ resource "aws_sns_topic_subscription" "error_email" {
   protocol  = "email"
   endpoint  = "46ki75@gmail.com"
 }
+
+resource "aws_sns_topic_subscription" "error_lambda" {
+  topic_arn = aws_sns_topic.error.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_alias.notion_notification.arn
+}

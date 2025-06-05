77a68c70676000640ff7aaf3acb53fdbca006454
Ikuma Yamashita <me@ikuma.cloud>
2025-06-05 16:19:16 +0000

    refactor: add review management functionality to AnkiStore and update related components

--- packages/web/components/Anki/Meta.vue
+++ packages/web/components/Anki/Meta.vue
@@ -1,30 +1,42 @@
 <template>
   <div class="queue" id="queue">
-    <Icon icon="mdi:academic-cap" class="icon" />
+    <Icon icon="mdi:academic-cap" class="icon" height="1rem" />
     <ElmInlineText :text="`Should Learn: ${ankiStore.getShouldLearnCount}`" />
 
-    <Icon icon="mdi:chart-bar-stacked" class="icon" />
+    <Icon icon="mdi:chart-bar-stacked" class="icon" height="1rem" />
     <ElmInlineText :text="`Queue: ${ankiStore.ankiList.length}`" />
+
+    <span v-if="ankiStore.getCurrentAnki.isReviewRequired" class="review-label">
+      Review Required
+    </span>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ElmInlineText } from '@elmethis/core'
-import { Icon } from '@iconify/vue'
-const ankiStore = useAnkiStore()
+import { ElmInlineText } from "@elmethis/core";
+import { Icon } from "@iconify/vue";
+const ankiStore = useAnkiStore();
 </script>
 
 <style scoped lang="scss">
-@use '../../scss/_mixins.scss';
+@use "../../scss/_mixins.scss";
 
 .icon {
-  @include mixins.icon;
+  color: #b69545;
 }
 
 .queue {
+  height: 2rem;
   display: flex;
   justify-content: flex-start;
   align-items: center;
   gap: 0.5rem;
+}
+
+.review-label {
+  color: #be5252;
+  padding: 0.25rem;
+  border: 1px solid #be5252;
+  border-radius: 2px;
 }
 </style>

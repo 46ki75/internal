41f1be5945e4b48616541f2d4e5c31aa30e302ba
Ikuma Yamashita <me@ikuma.cloud>
2025-06-18 07:41:47 +0000

    feat: add BookmarkList and BookmarkTag exports to index and update View component to use them

--- packages/web/components/Bookmark/View.vue
+++ packages/web/components/Bookmark/View.vue
@@ -4,42 +4,27 @@
     <ElmBlockFallback v-if="bookmarkStore.bookmarkList.length === 0" />
     <div class="global-fade-in" v-else>
       <div v-for="tag in bookmarkStore.tags" :style="{ marginBlock: '1rem' }">
-        <div v-if="tag">
-          <ElmInlineText :text="tag.name" :color="tag.color" />
-        </div>
-        <div class="bookmark-container">
-          <template
-            v-for="(bookmark, index) in bookmarkStore.getBookmarkListByTagId(
-              tag?.id
-            )"
-          >
-            <BookmarkIcon
-              :name="bookmark.name"
-              :favicon="bookmark.favicon"
-              :href="bookmark.url"
-              :notionUrl="bookmark.notionUrl"
-              :nsfw="bookmark.nsfw"
-            />
-          </template>
-        </div>
+        <BookmarkTag
+          v-if="tag"
+          :label="tag.name"
+          :color="tag.color"
+          :style="{ marginBlock: '1rem' }"
+        />
+        <BookmarkList
+          :bookmarks="
+            convertBookmarks(bookmarkStore.getBookmarkListByTagId(tag?.id))
+          "
+        />
       </div>
 
-      <div>
-        <ElmInlineText text="Untagged" color="gray" />
-      </div>
-      <div class="bookmark-container">
-        <template
-          v-for="(bookmark, index) in bookmarkStore.getUntaggedBookmarkList"
-        >
-          <BookmarkIcon
-            :name="bookmark.name"
-            :favicon="bookmark.favicon"
-            :href="bookmark.url"
-            :notionUrl="bookmark.notionUrl"
-            :nsfw="bookmark.nsfw"
-          />
-        </template>
-      </div>
+      <BookmarkTag
+        label="Untagged"
+        color="gray"
+        :style="{ marginBlock: '1rem' }"
+      />
+      <BookmarkList
+        :bookmarks="convertBookmarks(bookmarkStore.getUntaggedBookmarkList)"
+      />
     </div>
 
     <ElmParagraph v-if="bookmarkStore.error != null">{{
@@ -57,6 +42,12 @@ import {
 } from "@elmethis/core";
 import { useWindowFocus } from "@vueuse/core";
 
+import {
+  BookmarkTag,
+  BookmarkList,
+  type BookmarkListProps,
+} from "../../../components/src";
+
 const bookmarkStore = useBookmarkStore();
 
 onMounted(async () => {
@@ -70,6 +61,19 @@ watch(focused, async () => {
     await bookmarkStore.fetch();
   }
 });
+
+const convertBookmarks = (
+  bookmarks: Bookmark[]
+): BookmarkListProps["bookmarks"] => {
+  const results = bookmarks.map((bookmark) => ({
+    name: bookmark.name,
+    href: bookmark.url,
+    favicon: bookmark.favicon,
+    notionUrl: bookmark.notionUrl,
+    nsfw: bookmark.nsfw,
+  }));
+  return results;
+};
 </script>
 
 <style scoped lang="scss">

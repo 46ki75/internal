d899475dd4f312c47d5931dffba9f96f5f6ed9b1
Ikuma Yamashita <me@ikuma.cloud>
2025-08-09 17:15:14 +0000

    feat: integrate TypingTable component and update Typing interface types

--- packages/web/components/Typing/Create.vue
+++ packages/web/components/Typing/Create.vue
@@ -14,33 +14,39 @@
       <ElmButton :loading="typingStore.loading" block @click="handleSubmit">
         UPSERT
       </ElmButton>
+
+      <TypingTable
+        v-if="typingStore.typingList"
+        :data="typingStore.typingList"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ElmButton, ElmTextField } from '@elmethis/core'
+import { ElmButton, ElmTextField } from "@elmethis/core";
+import TypingTable from "../../../components/src/components/typing/TypingTable.vue";
 
-const typingStore = useTypingStore()
+const typingStore = useTypingStore();
 
-const text = ref('')
-const description = ref('')
+const text = ref("");
+const description = ref("");
 
 const handleSubmit = async () => {
-  await typingStore.upsert()
-  text.value = ''
-  description.value = ''
-  typingStore.setText('')
-  typingStore.setDescription('')
-}
+  await typingStore.upsert();
+  text.value = "";
+  description.value = "";
+  typingStore.setText("");
+  typingStore.setDescription("");
+};
 
 watch(text, () => {
-  typingStore.setText(text.value)
-})
+  typingStore.setText(text.value);
+});
 
 watch(description, () => {
-  typingStore.setDescription(description.value)
-})
+  typingStore.setDescription(description.value);
+});
 </script>
 
 <style scoped lang="scss">

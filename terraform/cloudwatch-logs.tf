0d88ae368b3cd4b3fa828a05e8e3d956a7e3b1cc
Ikuma Yamashita <me@ikuma.cloud>
2026-02-09 17:07:55 +0000

    feat: update CloudWatch log subscription filters for WARN and ERROR levels

--- terraform/cloudwatch-logs.tf
+++ terraform/cloudwatch-logs.tf
@@ -3,10 +3,17 @@ resource "aws_cloudwatch_log_group" "lambda_graphql" {
   retention_in_days = 30
 }
 
-resource "aws_cloudwatch_log_subscription_filter" "lambda_graphql_debug" {
-  name            = "${terraform.workspace}-46ki75-internal-cloudwatch-subscription_filter-lambda_graphql_debug"
+resource "aws_cloudwatch_log_subscription_filter" "lambda_graphql_warn" {
+  name            = "${terraform.workspace}-46ki75-internal-cloudwatch-subscription_filter-lambda_graphql_warn"
   log_group_name  = aws_cloudwatch_log_group.lambda_graphql.name
-  filter_pattern  = "{$.level=\"DEBUG\"}"
+  filter_pattern  = "{$.level=\"WARN\"}"
+  destination_arn = aws_lambda_function.reporter.arn
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "lambda_graphql_error" {
+  name            = "${terraform.workspace}-46ki75-internal-cloudwatch-subscription_filter-lambda_graphql_error"
+  log_group_name  = aws_cloudwatch_log_group.lambda_graphql.name
+  filter_pattern  = "{$.level=\"ERROR\"}"
   destination_arn = aws_lambda_function.reporter.arn
 }
 

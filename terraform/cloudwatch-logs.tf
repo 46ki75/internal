5f017b6ce913f446bba5d31e1be1b7cd396de241
Ikuma Yamashita <me@ikuma.cloud>
2026-02-09 16:19:20 +0000

    feat: add CloudWatch log subscription filter for lambda_graphql with DEBUG level

--- terraform/cloudwatch-logs.tf
+++ terraform/cloudwatch-logs.tf
@@ -3,6 +3,13 @@ resource "aws_cloudwatch_log_group" "lambda_graphql" {
   retention_in_days = 30
 }
 
+resource "aws_cloudwatch_log_subscription_filter" "lambda_graphql_debug" {
+  name            = "${terraform.workspace}-46ki75-internal-cloudwatch-subscription_filter-lambda_graphql_debug"
+  log_group_name  = aws_cloudwatch_log_group.lambda_graphql.name
+  filter_pattern  = "{$.level=\"DEBUG\"}"
+  destination_arn = aws_lambda_function.reporter.arn
+}
+
 resource "aws_cloudwatch_log_group" "lambda_reporter" {
   name              = "/${terraform.workspace}/46ki75/internal/cloudwatch/log_group/lambda_reporter"
   retention_in_days = 30

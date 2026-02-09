533ed6b74242960045f3b324287e5d11dabc9da9
Ikuma Yamashita <me@ikuma.cloud>
2026-02-09 14:13:45 +0000

    feat: implement icon page with data fetching and display

--- packages/web/pages/icon.vue
+++ packages/web/pages/icon.vue
@@ -1,11 +1,43 @@
 <template>
-  <div>Template</div>
+  <div>
+    <div>
+      <h1>Icon Page</h1>
+
+      <div v-for="icon of data">
+        <p>Icon Data:</p>
+        <pre>{{ icon.id }}</pre>
+        <img :src="icon.url" :alt="icon.id" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { openApiClient } from "~/openapi/client";
+
 export interface IconProps {}
 
 withDefaults(defineProps<IconProps>(), {});
+
+const authStore = useAuthStore();
+
+const { data } = useAsyncData("", async () => {
+  await authStore.refreshIfNeed();
+
+  if (authStore.session.accessToken == null) {
+    throw new Error("No access token");
+  }
+
+  const response = await openApiClient.GET("/api/v1/icon", {
+    params: {
+      header: {
+        Authorization: authStore.session.accessToken,
+      },
+    },
+  });
+
+  return response.data;
+});
 </script>
 
 <style module lang="scss"></style>

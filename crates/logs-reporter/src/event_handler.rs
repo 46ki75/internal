6e2eb7760f697d406fb105588a108e46e032bae3
Ikuma Yamashita <me@ikuma.cloud>
2026-02-09 16:19:25 +0000

    feat: add AWS SDK for SNS and update event handler for CloudWatch logs

--- crates/logs-reporter/src/event_handler.rs
+++ crates/logs-reporter/src/event_handler.rs
@@ -1,10 +1,13 @@
+use aws_config::BehaviorVersion;
 use aws_lambda_events::event::cloudwatch_logs::LogsEvent;
 use lambda_runtime::{tracing, Error, LambdaEvent};
 
 pub(crate) async fn function_handler(event: LambdaEvent<LogsEvent>) -> Result<(), Error> {
     // Extract some useful information from the request
-    let payload = event.payload;
+    let payload = event.payload.aws_logs.data;
     tracing::info!("Payload: {:?}", payload);
 
+    let sdk_config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+    let client = aws_sdk_sns::Client::new(&sdk_config);
     Ok(())
 }

13089b1992902684babdc4c6270fb33f67d6bf49
Ikuma Yamashita <me@ikuma.cloud>
2026-04-06 23:51:25 +0900

    feat(icon): add content_type field to IconEntiry and IconResponse

--- crates/graphql/src/icon/entity.rs
+++ crates/graphql/src/icon/entity.rs
@@ -2,4 +2,5 @@ pub struct IconEntiry {
     pub id: String,
     pub url: String,
     pub name: String,
+    pub content_type: Option<String>,
 }
--- crates/graphql/src/icon/response.rs
+++ crates/graphql/src/icon/response.rs
@@ -7,6 +7,7 @@ pub struct IconResponse {
     pub id: String,
     pub url: String,
     pub name: String,
+    pub content_type: Option<String>,
 }
 
 impl From<IconEntiry> for IconResponse {
@@ -15,6 +16,7 @@ impl From<IconEntiry> for IconResponse {
             id: value.id,
             url: value.url,
             name: value.name,
+            content_type: value.content_type,
         }
     }
 }

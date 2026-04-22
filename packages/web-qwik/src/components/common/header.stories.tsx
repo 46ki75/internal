1a7c6fda8c95c561fbf9ad8fa3e6f489f92b63f9
Ikuma Yamashita <me@ikuma.cloud>
2026-04-23 06:46:14 +0900

    feat: add link support to Header component and integrate MDI icons

--- packages/web-qwik/src/components/common/header.stories.tsx
+++ packages/web-qwik/src/components/common/header.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "storybook-framework-qwik";
 import { Header, type HeaderProps } from "./header";
+import { $ } from "@builder.io/qwik";
+import { mdiHome, mdiTag } from "@mdi/js";
 
 const meta: Meta<HeaderProps> = {
   title: "Components/Common/header",
@@ -11,4 +13,11 @@ const meta: Meta<HeaderProps> = {
 export default meta;
 type Story = StoryObj<HeaderProps>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  args: {
+    links: [
+      { d: mdiHome, onClick$: $(() => console.log("Home clicked")) },
+      { d: mdiTag, onClick$: $(() => console.log("Tag clicked")) },
+    ],
+  },
+};
--- packages/web-qwik/src/components/common/header.tsx
+++ packages/web-qwik/src/components/common/header.tsx
@@ -1,19 +1,36 @@
-import { component$, type CSSProperties } from "@builder.io/qwik";
+import {
+  component$,
+  QRLEventHandlerMulti,
+  type CSSProperties,
+} from "@builder.io/qwik";
 
 import styles from "./header.module.css";
-import { ElmToggleTheme } from "@elmethis/qwik";
+import { ElmMdiIcon, ElmToggleTheme } from "@elmethis/qwik";
 
 export interface HeaderProps {
   class?: string;
 
   style?: CSSProperties;
+
+  links: Array<{
+    d: string;
+    onClick$: QRLEventHandlerMulti<PointerEvent, Element>;
+  }>;
 }
 
-export const Header = component$<HeaderProps>(({ class: className, style }) => {
-  return (
-    <header class={[styles["header"], className]} style={style}>
-      <div></div>
-      <ElmToggleTheme />
-    </header>
-  );
-});
+export const Header = component$<HeaderProps>(
+  ({ class: className, style, links }) => {
+    return (
+      <header class={[styles["header"], className]} style={style}>
+        <div class={styles["link-container"]}>
+          {links.map((link, index) => (
+            <div class={styles["link"]} key={index} onClick$={link.onClick$}>
+              <ElmMdiIcon d={link.d} size="1.75rem" />
+            </div>
+          ))}
+        </div>
+        <ElmToggleTheme />
+      </header>
+    );
+  },
+);

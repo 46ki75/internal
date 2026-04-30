cced9195dae8cbb5b14e53fa41055f316fbb3db7
Ikuma Yamashita <me@ikuma.cloud>
2026-05-01 02:29:48 +0900

    fix: update ECR repository name to include workspace for better environment isolation

--- terraform/bedrock-agentcore.tf
+++ terraform/bedrock-agentcore.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "ag-ui-server" {
-  name                 = "ag-ui-server"
+  name                 = "${terraform.workspace}/ag-ui-server"
   image_tag_mutability = "MUTABLE"
 }
 

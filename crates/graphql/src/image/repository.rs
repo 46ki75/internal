7bff2454b7c11a0c31d694b700580efa19a04805
Ikuma Yamashita <me@ikuma.cloud>
2026-05-02 02:01:38 +0900

    feat: implement fetch_images method in ImageRepository trait

--- crates/graphql/src/image/repository.rs
+++ crates/graphql/src/image/repository.rs
@@ -1,3 +1,22 @@
-pub trait ImageRepository {}
+use std::pin::Pin;
+
+pub trait ImageRepository {
+    fn fetch_images(
+        &self,
+    ) -> Pin<Box<dyn Future<Output = Result<Vec<super::dto::ImageDto>, crate::error::Error>> + Send>>;
+}
 
 pub struct ImageRepositoryImpl {}
+
+// impl ImageRepository for ImageRepositoryImpl {
+//     fn fetch_images(
+//         &self,
+//     ) -> Pin<Box<dyn Future<Output = Result<Vec<super::dto::ImageDto>, crate::error::Error>> + Send>>
+//     {
+//         Box::pin(async move {
+//             let notionrs_client = crate::cache::get_or_init_notionrs_client().await?;
+
+//             todo!();
+//         })
+//     }
+// }

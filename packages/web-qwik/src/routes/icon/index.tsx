dddbd94b84ceb601502592774f62d54765bcd6f1
Ikuma Yamashita <me@ikuma.cloud>
2026-05-05 23:25:15 +0900

    feat: enhance icon display with content type grouping and inline text rendering

--- packages/web-qwik/src/routes/icon/index.tsx
+++ packages/web-qwik/src/routes/icon/index.tsx
@@ -6,7 +6,7 @@ import {
 } from "@builder.io/qwik";
 
 import styles from "./icon.module.css";
-import { useAsyncState } from "@elmethis/qwik";
+import { ElmInlineText, useAsyncState } from "@elmethis/qwik";
 import { AuthContext } from "~/context/auth-context";
 import { openApiClient } from "~/openapi/client";
 import { IconCell } from "~/components/icon/icon-cell";
@@ -36,22 +36,46 @@ export default component$<IndexProps>(({ class: className, style }) => {
         throw new Error("No data");
       }
 
-      return res.data;
+      const results: Record<string, typeof res.data> = {
+        unknown: [],
+      };
+
+      for (const icon of res.data) {
+        if (icon.content_type == null) {
+          results.unknown.push(icon);
+        } else if (icon.content_type in results) {
+          results[icon.content_type].push(icon);
+        } else {
+          results[icon.content_type] = [icon];
+        }
+      }
+
+      return results;
     }),
-    [],
+    {},
     { immediate: true },
   );
 
   return (
     <div class={[styles["icon"], className]} style={style}>
-      {state.value.map((icon) => (
-        <>
-          <IconCell
-            mimeType={icon.content_type ?? undefined}
-            src={icon.url}
-            name={icon.name}
-          />
-        </>
+      {Object.entries(state.value).map(([contentType, icons]) => (
+        <div key={contentType} class={styles["icon-content-type-group"]}>
+          <div class={styles["icon-content-type-group-header"]}>
+            <div class={styles["spacer"]}></div>
+            <ElmInlineText code>{contentType}</ElmInlineText>
+            <div class={styles["spacer"]}></div>
+          </div>
+          <div class={styles["icon-group"]}>
+            {icons.map((icon) => (
+              <IconCell
+                key={icon.id}
+                mimeType={icon.content_type ?? undefined}
+                src={icon.url}
+                name={icon.name}
+              />
+            ))}
+          </div>
+        </div>
       ))}
     </div>
   );

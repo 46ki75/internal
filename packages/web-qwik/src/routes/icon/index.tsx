19ded67e5a999df24631440b456e7f1b11100e27
Ikuma Yamashita <me@ikuma.cloud>
2026-05-07 00:01:28 +0900

    feat: add fuse.js for enhanced search functionality in icon component

--- packages/web-qwik/src/routes/icon/index.tsx
+++ packages/web-qwik/src/routes/icon/index.tsx
@@ -1,15 +1,22 @@
 import {
   $,
   component$,
+  NoSerialize,
+  noSerialize,
+  useComputed$,
   useContext,
+  useSignal,
   type CSSProperties,
 } from "@builder.io/qwik";
 
 import styles from "./icon.module.css";
-import { ElmInlineText, useAsyncState } from "@elmethis/qwik";
+import { ElmInlineText, ElmTextField, useAsyncState } from "@elmethis/qwik";
 import { AuthContext } from "~/context/auth-context";
 import { openApiClient } from "~/openapi/client";
 import { IconCell } from "~/components/icon/icon-cell";
+
+import Fuse from "fuse.js";
+import { paths } from "~/openapi/schema";
 
 export interface IndexProps {
   class?: string;
@@ -17,8 +24,13 @@ export interface IndexProps {
   style?: CSSProperties;
 }
 
+type Icon =
+  paths["/api/v1/icon"]["get"]["responses"]["200"]["content"]["application/json"][number];
+
 export default component$<IndexProps>(({ class: className, style }) => {
   const authStore = useContext(AuthContext);
+  const searchKeyword = useSignal<string>("");
+  const fuseInstance = useSignal<NoSerialize<Fuse<Icon>> | null>(null);
 
   const { state } = useAsyncState(
     $(async () => {
@@ -35,6 +47,8 @@ export default component$<IndexProps>(({ class: className, style }) => {
       if (!res.data) {
         throw new Error("No data");
       }
+
+      fuseInstance.value = noSerialize(new Fuse(res.data, { keys: ["name"] }));
 
       const results: Record<string, typeof res.data> = {
         unknown: [],
@@ -56,8 +70,41 @@ export default component$<IndexProps>(({ class: className, style }) => {
     { immediate: true },
   );
 
+  const searchResults = useComputed$(() => {
+    if (searchKeyword.value.trim() === "") return [];
+    if (!fuseInstance.value) return [];
+    return fuseInstance.value
+      .search(searchKeyword.value)
+      .map((result) => result.item);
+  });
+
   return (
     <div class={[styles["icon"], className]} style={style}>
+      <ElmTextField
+        label="Keyword"
+        onInput$={(event, element) => {
+          searchKeyword.value = element.value;
+        }}
+      />
+
+      <div class={styles["icon-content-type-group"]}>
+        <div class={styles["icon-content-type-group-header"]}>
+          <div class={styles["spacer"]}></div>
+          <ElmInlineText code>Search Results</ElmInlineText>
+          <div class={styles["spacer"]}></div>
+        </div>
+        <div class={styles["icon-group"]}>
+          {searchResults.value.map((icon) => (
+            <IconCell
+              key={icon.id}
+              mimeType={icon.content_type ?? undefined}
+              src={icon.url}
+              name={icon.name}
+            />
+          ))}
+        </div>
+      </div>
+
       {Object.entries(state.value).map(([contentType, icons]) => (
         <div key={contentType} class={styles["icon-content-type-group"]}>
           <div class={styles["icon-content-type-group-header"]}>

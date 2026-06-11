ca492385a15fb31cb948e35e9b8a97faa5a84ce1
Ikuma Yamashita <me@ikuma.cloud>
2026-06-12 00:11:29 +0900

    refactor: update font handling across components and stylesheets

--- packages/web-qwik/src/components/router-head/router-head.tsx
+++ packages/web-qwik/src/components/router-head/router-head.tsx
@@ -16,6 +16,17 @@ export const RouterHead = component$(() => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,400;0,500;1,400;1,500&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Zen+Kaku+Gothic+New:wght@300;400;500;700;900&display=swap"
+      />
+
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
       ))}
